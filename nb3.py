40b15f859e54d3ca902ec6c82fa49b6ec3bd8178
Tiago Antao <tiagoantao@gmail.com>
2014-02-02 17:05:49 +0000

    less lake trout

--- nb3.py
+++ nb3.py
@@ -13,6 +13,7 @@ N0s, sampCohort, sampSize, sampSNP, numGens, reps, dataDir = getVarConf(cf)
 models = N0s.keys()
 models.sort()
 
+
 def dumpOut(gen, born, curr, prev, parents, gender, cfg):
     cntPossParents = 0
     cntPossDads = 0
@@ -27,42 +28,42 @@ def dumpOut(gen, born, curr, prev, parents, gender, cfg):
             cntOfs += 1
     for pi in prev:
 
-        age = (gen-1) - born[pi]
-        cntPossParents +=1
+        age = (gen - 1) - born[pi]
+        cntPossParents += 1
 
-        if gender[pi]==1:
-            fec = cfg.fecundityMale
-            cntPossDads +=1
+        if gender[pi] == 1:
+            #fec = cfg.fecundityMale
+            cntPossDads += 1
             ofsCntDads[pi] = 0
         else:
-            fec = cfg.fecundityFemale
-            cntPossMoms +=1
+            #fec = cfg.fecundityFemale
+            cntPossMoms += 1
             ofsCntMoms[pi] = 0
         ofsCnt[pi] = 0
     for parent in parents:
         #print prev.index(parent)
-        ofsCnt[parent] +=1
+        ofsCnt[parent] += 1
         if gender[parent] == 1:
             ofsCntDads[parent] += 1
         else:
             ofsCntMoms[parent] += 1
-    kbar = 2.0*cntOfs/cntPossParents
+    kbar = 2.0 * cntOfs / cntPossParents
     a = scipy.array(ofsCnt.values())
     vk = a.var()
 
-    kbarm = 1.0*cntOfs/cntPossDads
+    kbarm = 1.0 * cntOfs / cntPossDads
     a = scipy.array(ofsCntDads.values())
     vkm = a.var()
 
-    kbarf = 1.0*cntOfs/cntPossMoms
+    kbarf = 1.0 * cntOfs / cntPossMoms
     a = scipy.array(ofsCntMoms.values())
     vkf = a.var()
-    nb = (kbar*cntPossParents-2)/(kbar-1+vk/kbar)
+    nb = (kbar * cntPossParents - 2) / (kbar - 1 + vk / kbar)
     return cntOfs, cntPossParents, len(set(parents)), kbar, vk, kbarm, vkm, kbarf, vkf, nb
 
 
 def doModel(model, N0, rep, cfg):
-    f = bz2.BZ2File("data/%s/%d%s%d.sim.bz2" %(dataDir, N0, model, rep))
+    f = bz2.BZ2File("data/%s/%d%s%d.sim.bz2" % (dataDir, N0, model, rep))
     myStart = 50
     currGen = 0
     gender = {}
@@ -74,8 +75,8 @@ def doModel(model, N0, rep, cfg):
     for l in f:
         toks = l.rstrip().split(" ")
         gen = int(toks[0])
-        if gen>currGen:
-            if currGen>=myStart:
+        if gen > currGen:
+            if currGen >= myStart:
                 res[gen] = dumpOut(gen, born, curr, prev, parents, gender, cfg)
             prev = curr
             curr = []
@@ -87,12 +88,13 @@ def doModel(model, N0, rep, cfg):
         father = int(float(toks[4]))
         mother = int(float(toks[5]))
         curr.append(id)
-        if age==1:
+        if age == 1:
             gender[id] = sex
             parents.extend([father, mother])
             born[id] = gen
     f.close()
     return res
+
 
 def dumpMeans(allRes):
     keys = allRes[0].keys()
@@ -111,7 +113,7 @@ def dumpMeans(allRes):
                 acu[j] += myRes[j]
         print key,
         for i in range(numVals):
-            v = acu[i]/len(allRes)
+            v = acu[i] / len(allRes)
             print v,
             vals[i].append(v)
         print
@@ -145,11 +147,11 @@ for model in models:
             continue
         vals = dumpMeans(allRes)
         print "",
-        for i in range(len(vals)-1):
+        for i in range(len(vals) - 1):
             print scipy.mean(vals[i]),
         try:
             hm = stats.hmean(vals[-1])
             print hm
         except ValueError:
             print "Negs",
-            print stats.hmean(filter(lambda x: x > 0,vals[-1]))
+            print stats.hmean(filter(lambda x: x > 0, vals[-1]))
